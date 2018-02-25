093b1da5164b103502ed75672a782313c38be2f2
KyrgyzBoy <daniyarkushaliev@gmail.com>
2018-02-24 18:23:11 -0700

    Actual Initial Commit

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMove : MonoBehaviour {
+
+	public Transform target;
+	public Transform myTransform;
+	public float EnemySpeed;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (target);
+		transform.Translate (Vector3.forward * EnemySpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject[] enemies;
+	public Vector3 spawnValues;
+	public float spawnWait;
+	public float SpawnMostWait;
+	public float SpawnLeastWait;
+	public int startWait;
+	public bool stop;
+
+	int randEnemy;
+
+	void Start () {
+		StartCoroutine (waitSpawner());
+	}
+	
+
+	void Update () {
+		spawnWait = Random.Range (SpawnLeastWait, SpawnMostWait);
+	}
+
+	IEnumerator waitSpawner(){
+		yield return new WaitForSeconds(startWait);
+
+		while (!stop) {
+			randEnemy = Random.Range (0, 2);
+
+			Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), 0, 
+				Random.Range (-spawnValues.z, spawnValues.z));
+
+			Instantiate (enemies [randEnemy], spawnPosition + transform.TransformPoint(0, 0, 0), 
+				gameObject.transform.rotation);
+
+			yield return new WaitForSeconds (spawnWait);
+		}
+	}
+}
